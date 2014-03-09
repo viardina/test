a42c9b689ac3e34b914855005f9ffbfb9d74fb13
Pasquale Iaquinta <info@es-dev.it>
2014-03-09 11:53:14 +0100

    456

---
+++ Form1.Designer.vb
@@ -0,0 +1,29 @@
+Imports Gizmox.WebGUI.Forms
+Imports Gizmox.WebGUI.Common
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits Gizmox.WebGUI.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Visual WebGui Designer
+    Private Shadows components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Visual WebGui Designer
+    'It can be modified using the Visual webGui Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        components = New System.ComponentModel.Container()
+        Me.Text = "Form1"
+    End Sub
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,7 @@
+Imports Gizmox.WebGUI.Forms
+Imports Gizmox.WebGUI.Common
+
+Public Class Form1
+
+
+End Class
